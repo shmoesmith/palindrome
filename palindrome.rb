7b042c0491ef9bc82ce63039f2d4bac2c54184df
Alan Vincent <alan.d.vincent@gmail.com>
2017-06-09 10:00:52 -0600

    added a 3rd way to do it

--- palindrome.rb
+++ palindrome.rb
@@ -1,11 +1,8 @@
-@test_word = ''
-@second_word = ''
-@new_arry=[1]
+
 def input
     puts "enter the word you would like to check to see if it is a palindrome"
     user_word = gets.strip
-    @test_word = user_word
-    break_it_up
+    break_it_up(user_word)
 end
 
 
@@ -19,17 +16,19 @@ end
 #     test_it
 #end
 
-def break_it_up
-    @arry=@test_word.split('')
-    @arry.each do |letter|
-    @second_word = letter + @second_word
+def break_it_up(word)
+    user_word = word
+    arry = user_word.split('')
+    second_word = ''
+    arry.each do |letter|
+    second_word = letter + second_word
     end
-    @second_word.strip
-    test_it
+    second_word.strip
+    test_it(user_word,second_word)
 end
 
-def test_it
-    if @test_word == @second_word
+def test_it(first_word, new_word)
+    if first_word == new_word
         puts "word is a palindrome"
     else
         puts "word not a palindrome"
